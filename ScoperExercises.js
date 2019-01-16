082e1f98780c1e16b2f0f0e671b8ef8bc4624367
Abdul Atif <abdul.pesit@gmail.com>
2019-01-16 10:25:02 +0400

    scope of the variable tested

---
+++ ScoperExercises.js
@@ -0,0 +1,31 @@
+var top = 10;
+var inner = 50;
+
+function foo () {
+  var inner = 20;
+  console.log(inner);
+}
+
+
+foo();
+console.log(inner);
+
+
+function myFu () {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+
+}
+myFu();
+
+
+var name = "Java Programming";
+
+function printGreeting (name) {
+   console.log("Hello " +  name);
+}
+
+
+printGreeting("UAE");
+printGreeting();
