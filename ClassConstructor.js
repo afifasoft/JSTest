b2e91ca61cd3c17d8faa8e39f3725565461dfe67
afifasoft <abdul.pesit@gmail.com>
2019-02-27 00:15:18 +0400

    Class ctr and inheritance

---
+++ ClassConstructor.js
@@ -0,0 +1,18 @@
+class CoolGuy {
+  specialTrick = nothing;
+
+  CoolGuy(trick) {
+
+    specialTrick = trick;
+
+  }
+
+  showOff() {
+      output( "Here's my trick: ", specialTrick );
+  }
+
+}
+
+
+Joe = new CoolGuy( "jumping rope" );
+Joe.showOff(); // Here's my trick: jumping rope
